327a32b648c9488d01ea71c7fcca80e108802607
dream-mo <dreammovip@163.com>
2018-10-29 16:36:56 +0800

    Create docker-centos-install.sh
    cenos下安装docker的shell脚本

---
+++ install/docker-centos-install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-selinux \
+                  docker-engine-selinux \
+                  docker-engine
+
+yum install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2
+
+yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+
+yum-config-manager --enable docker-ce-edge
+
+yum-config-manager --enable docker-ce-test
+
+yum-config-manager --disable docker-ce-edge
+
+yum update -y
+
+yum install -y docker-ce
+
+mkdir -p /etc/docker/
+
+touch /etc/docker/daemon.json
+
+echo {'"registry-mirrors"': ['"https://registry.docker-cn.com"']} > /etc/docker/daemon.json #加入国内镜像加速地址
+
+service docker restart
+
+echo -e "install success"
+
+docker version # 查看docker版本
